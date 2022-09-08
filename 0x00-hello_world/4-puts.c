1189712eb22d1496943a9995bb041caa1b8435d1
Donson1 <111167240+Donson1@users.noreply.github.com>
2022-09-08 22:49:22 +0000

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* main - print the string in the puts function */
+
+int main(void)
+{
+  /* prints ...*/
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
