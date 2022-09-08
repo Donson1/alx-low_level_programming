b7ba58c5da451609747c79a637dca4c362fc9b03
Donson1 <111167240+Donson1@users.noreply.github.com>
2022-09-08 23:06:29 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/* main - print the string in the puts function 
+/* 
+*main - print the string in the puts function 
 *
-* Description: using of main function
-* the program prints "Programming is like building a multilingual puzzle
+*Description: using of main function
+*the program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
