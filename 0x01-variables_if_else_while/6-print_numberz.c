cf83b9a2ae049d4bf40cf1292055dc01c1533a7f
Donson1 <111167240+Donson1@users.noreply.github.com>
2022-09-10 03:14:58 +0000

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Success
+*/
+int main(void)
+{
+int a = 0;
+
+for (; a < 10; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
