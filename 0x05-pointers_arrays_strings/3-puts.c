617f3fa1dea036fa1662fdb4d4da868249df7f68
Donson1 <111167240+Donson1@users.noreply.github.com>
2022-09-20 03:10:30 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - print a string to stdout
- * @str: char array string type
- * Description: Can only use _putchar
- */
+* _puts - print a string to stdout
+* @str: char array string type
+* Description: Can only use _putchar
+*/
 
 void _puts(char *str)
 {
